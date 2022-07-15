e5285166eb34e8593f9a79ffa86d25ec1ab14aee
arslanberk <berk.arslan93@gmail.com>
2022-07-15 05:28:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# INPUT PARSER
+
+```bash
+python3 inputparser.py -i input.txt -o out.txt
+```
+
+| args | description |
+| ------ | ------ |
+| -i | Input file. Sample file is included. |
+| -o | Output file. This file will be removed if it exists when script runs. |
---
+++ inputparser.py
@@ -0,0 +1,80 @@
+import os, sys, getopt
+
+KEY_ID      = "[ID]"
+KEY_PRIMARY = "[PRIMARY]"
+KEY_EVOLUTIONARY = "[EVOLUTIONARY]"
+
+
+def write_to_file(outputFile, lineContent):
+   with open(outputFile, 'a') as the_file:
+      the_file.write(lineContent+'\n')
+   
+def prepare_id_line(idItems):
+   text = '"'
+   text += '","'.join(idItems);
+   return text + '","'
+   
+def parse_input(inputFile, outputFile):
+   try:
+      isData = False
+      idItems = []
+      dataRows = []
+      with open(inputFile, 'r', encoding='UTF-8') as file:
+         while (l := file.readline().rstrip()):
+            if KEY_ID in l or KEY_PRIMARY in l:
+               isData = False
+               dataRows.append(l)
+            elif KEY_EVOLUTIONARY in l:
+               isData = True
+               dataRows.append(l)
+               write_to_file(outputFile, prepare_id_line(idItems))
+               idItems = []
+               for d in dataRows:
+                  write_to_file(outputFile, d)
+               dataRows = []
+            else:
+               if not isData:
+                  idItems.append(l)
+                  dataRows.append(l)
+               else:
+                  if "+" in l:
+                     l += '"'
+                  write_to_file(outputFile, l)
+               
+   except Exception as ex:
+      print(ex)
+      sys.exit(2)
+   
+
+def print_help(file):
+   print(file + ' -i <inputfile> -o <outputfile>')
+
+
+def main(file, argv):
+   inputFile = None
+   outputFile = None
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print_help(file)
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print_help(file)
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputFile = arg
+      elif opt in ("-o", "--ofile"):
+         outputFile = arg
+   if not inputFile or not outputFile:
+      print_help(file)
+      sys.exit()
+   try:
+      os.remove(outputFile)
+   except OSError:
+      pass
+   parse_input(inputFile, outputFile)
+   
+    
+if __name__ == "__main__":
+    main(sys.argv[0],sys.argv[1:])
